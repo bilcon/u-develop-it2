e363e14f48aedd9af750b3a4249016b962972439
Bill Morris <bilcon@swbell.net>
2022-05-20 13:56:15 -0500

    add query statements

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const mysql = require("mysql2");
 const express = require("express");
 const res = require("express/lib/response");
+const { param } = require("express/lib/request");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -21,8 +22,36 @@ const db = mysql.createConnection(
   console.log("Connected to the election2 database.")
 );
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-  console.log(rows);
+//db.query(`SELECT * FROM candidates`, (err, rows) => {
+//console.log(rows)
+//})
+
+// Get a single candidate
+//db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//if (err) {
+//console.log(err);
+//}
+//console.log(row);
+//});
+
+// Delete a candidate
+//db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//if (err) {
+//console.log(err);
+//}
+//console.log(result);
+//});//
+
+//Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+              VALUES (?,?,?,?)`;
+const params = [1, "Ronald", "Firbank", 1];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
 });
 
 //Default response for any other request (Not Found)
